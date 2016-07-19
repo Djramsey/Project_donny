f220497caf88c2729de3a7354c94a239c4268bac
ScriptEd <scripted@ScriptEd-C02G1066DNKY.local>
2016-07-19 15:04:02 -0400

    2nd init

---
+++ sidebar.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @package _tk
+ */
+?>
+
+	</div><!-- close .main-content-inner -->
+
+	<div class="sidebar col-sm-12 col-md-4">
+
+	/<?php // add the class "panel" below here to wrap the sidebar in Bootstrap style ;) ?>
+		
+
+			<?php endif; ?>
+
+		</div><!-- close .sidebar-padder -->
